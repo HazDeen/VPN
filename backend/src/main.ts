cedada4a507c3690b92de7bc3589e1ad0f0729e6
HazDeen <maxzavalov90@gmail.com>
2026-02-15 07:42:51 +0300

    back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,11 +9,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['*'],
-  });
+  origin: [
+    'https://vpn-frontend-5kn.pages.dev',  // Cloudflare
+    'https://vpn-frontend.netlify.app',
+    'https://web.telegram.org',
+    'https://telegram.org',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['*'],
+});
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
